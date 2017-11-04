4950364718fb510aea52ed202b6486d97cee85df
Andrea Altea <oni.zerone@gmail.com>
2017-11-04 01:02:21 +0100

    Feat: define Model

---
+++ BookShelf/Model/Model.swift
@@ -0,0 +1,15 @@
+//
+//  Model.swift
+//  BookShelf
+//
+//  Created by Andrea Altea on 04/11/17.
+//  Copyright © 2017 Studiout. All rights reserved.
+//
+
+import UIKit
+
+class Model: NSObject {
+
+    let shared: Model = Model()
+    
+}
